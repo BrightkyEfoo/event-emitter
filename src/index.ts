e04d34d2f4fc8ddc628eda8fd00e2f3c04f693b0
BrightkyEfoo <brightefoo@gmail.com>
2023-12-07 00:45:23 +0100

    add once method

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,54 @@
+type callbackObj = {
+  callback: Function;
+  once: boolean;
+};
+
 class BrightkyEmitter {
   private _state: {
     [p: string | symbol]: any;
   } = {};
 
   private _callbacks: {
-    [p: string | symbol]: Function[] | undefined;
+    [p: string | symbol]: callbackObj[] | undefined;
+  } = {};
+
+  private _onceCallbacks: {
+    [p: string | symbol]: callbackObj[] | undefined;
   } = {};
 
   private _stateProxy = new Proxy(this._state, {
     set: (target, property, newValue, receiver) => {
       target[property] = newValue;
-      this._callbacks[property]?.forEach((callback) => callback(newValue));
+      this._callbacks[property]?.forEach((el) => {
+        el.callback(newValue)
+      });
+      let canRemove = false
+      this._onceCallbacks[property]?.forEach(el => {
+        el.callback(newValue)
+      })
+      if(canRemove) this._onceCallbacks[property] = undefined
       return true;
     },
   });
 
   on(eventName: string, callback: Function) {
     if (Array.isArray(this._callbacks[eventName])) {
-      this._callbacks[eventName]!.push(callback);
+      this._callbacks[eventName]!.push({ callback, once: false });
     } else {
-      this._callbacks[eventName] = [callback];
+      this._callbacks[eventName] = [{ callback, once: false }];
     }
   }
 
   emit(eventName: string, data: any) {
     this._stateProxy[eventName] = data;
+  }
+
+  once(eventName: string, callback: Function) {
+    if (Array.isArray(this._onceCallbacks[eventName])) {
+      this._onceCallbacks[eventName]!.push({ callback, once: true });
+    } else {
+      this._onceCallbacks[eventName] = [{ callback, once: true }];
+    }
   }
 }
 
